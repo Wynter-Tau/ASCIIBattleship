2dd5c24f1a4fbb8eda230fb91776826ff3266a57
Wynter-Tau <33078460+Wynter-Tau@users.noreply.github.com>
2021-01-15 10:01:29 -0800

    split checkBounds out of isValidGuess, add function for placing ship

--- Board.java
+++ Board.java
@@ -29,20 +29,51 @@ public class Board {
 
     }
 
-    public boolean isValidGuess(int x, int y) {
+    // function for adding ship to the board, length is length of the ship, isVeritcal is wether it is oriented vertically
+    // x and y are start coordinates, which should be the leftmost or topmost point of the ship, depending on if it's vertical or not
+    // if isVertical is true, the ship is built downwards from there,
+    // if isVertical is false then the ship is built to the right from there
+    // returns true if placement succeeded without issue, false otherwise
+    public boolean placeShip(int length, boolean isVertical, int x, int y) {
 
-        isValid = false;
+        // boolean to return at end of function, will be made true if successful
+        boolean success = false;
 
-        // check if space is within x bound
-        if(x < 9) {
+        // set of spaces planned to be used for the ship
+        int[][] plannedSpaces = new int[length][2];
 
-            // check if space is within y bound
-            if(y < 9) {
+        // fill in plannedSpaces based on wether ship is vertical or horizontal
+        for(int i = 0; i < length; i++) {
 
-                // check that the space hasn't been guessed already
-                if(display[x][y] == '~') {
+            if(isVertical) {
 
-                    isValid = true;
+                plannedSpaces[i][0] = x;
+                plannedSpaces[i][1] = y + i;
+
+            } else {
+
+                plannedSpaces[i][0] = x + i;
+                plannedSpaces[i][1] = y;
+
+            }
+
+        }
+
+        int numOfValidSpaces = 0;
+
+        // now that plannedSpaces is full, check that each one is within bounds and unoccupied
+        for(int i = 0; i < length; i++) {
+
+            int currentX = plannedSpaces[i][0];
+            int currentY = plannedSpaces[i][1];
+
+            // check that the space is on the board
+            if(checkBounds(currentX, currentY)) {
+
+                // check that the space is false (unoccupied) in data array
+                if(!data[currentX][currentY]) {
+
+                    numOfValidSpaces++;
 
                 }
 
@@ -50,7 +81,63 @@ public class Board {
 
         }
 
+        // if these two are equal, then the ship is valid and can be placed
+        if(numOfValidSpaces == length) {
+
+            for(int i = 0; i < length; i++) {
+
+                int currentX = plannedSpaces[i][0];
+                int currentY = plannedSpaces[i][1];
+
+                data[currentX][currentY] = true;
+
+            }
+
+        }
+
+        success = (numOfValidSpaces == length);
+
+        return success;
+
+    }
+
+    // checks if the given guess is a valid space for the player to guess
+    public boolean isValidGuess(int x, int y) {
+
+        boolean isValid = false;
+
+        // checks if the guess is in bounds
+        if(checkBounds(x, y)) {
+
+            // checks if the guess is an unKnown space
+            if(display[x][y] == '~') {
+
+                isValid = true;
+
+            }
+
+        }
+
         return isValid;
+
+    }
+
+    // checks if coordinates given are within the board;
+    public boolean checkBounds(int x, int y) {
+
+        boolean inBounds = false;
+
+        if((x < 9) && (x > -1)) {
+
+            if((y < 9) && (y > -1)) {
+
+                inBounds = true;
+
+            }
+
+        }
+
+        return inBounds;
 
     }
 
